80facd32886cd03cc7d6222779406d89861a8ec1
Vyacheslav Lopachev <vmlopach@gmail.com>
2018-12-18 23:32:39 +0600

    refactoring

--- src/com/epam/sequence/Sequence.java
+++ src/com/epam/sequence/Sequence.java
@@ -1,59 +1,40 @@
 package com.epam.sequence;
 
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Sequence {
-    static String sequence1 = "1100010101";
-    static String sequence2 = "1010001101";
+    private final static String SEQUENCE_1 = "1100010101100111000101011001";
+    private final static String SEQUENCE_2 = "1010001101100111000101011001";
+    private final static Pattern PATTERN = Pattern.compile("10.+11+");
 
     public static void main(String[] args) {
 
-        if (checkSequences(sequence1, sequence2, "0+") &&
-                checkSequences(sequence1, sequence2, "1+")){
-            System.out.println("массивы равны");
-            if (sequence1.equals(reverse(sequence2))){
-                System.out.println("Последовательности равны");
-            }
 
-            SortedMap<Integer, String[]> variants =
-                    getOptimalReversingSequence(sequence1, sequence2);
+        Matcher matcher = PATTERN.matcher(SEQUENCE_1);
 
-            String var1 = variants.get(variants.firstKey())[0];
-            String var2 = variants.get(variants.firstKey())[1];
-
-            int key = variants.firstKey();
-
-            System.out.println(var1);
-            System.out.println(var2);
-            System.out.println(key);
-
-        } else {
-            System.out.println("Последовательности не могут быть приведены, " +
-                    "т.к. последовательности 0 и 1 не совпадают");
+        while (matcher.find()){
+            System.out.println(SEQUENCE_1);
+            System.out.println(matcher.replaceFirst(reverse(matcher.group())));
         }
+
+
+
+//        if (checkSequences(SEQUENCE_1, SEQUENCE_2, "0+") &&
+//                checkSequences(SEQUENCE_1, SEQUENCE_2, "1+")){
+//
+//
+//
+//        } else {
+//            System.out.println("Последовательности не могут быть приведены, " +
+//                    "т.к. последовательности 0 и 1 не совпадают");
+//        }
     }
 
     private static String reverse(String message){
         StringBuilder sb = new StringBuilder(message);
         return sb.reverse().toString();
-    }
-
-    private static SortedMap<Integer, String[]> getOptimalReversingSequence(String sequence1, String sequence2){
-        SortedMap<Integer, String[]> variants = new TreeMap<>();
-        String[] sequences = {sequence1, sequence2};
-        variants.put(subtractionSequences(sequences), sequences);
-        sequences = new String[] {reverse(sequences[0]), reverse(sequences[1])};
-        variants.put(subtractionSequences(sequences), sequences);
-        sequences = new String[] {reverse(sequence1), sequence2};
-        variants.put(subtractionSequences(sequences), sequences);
-        sequences = new String[] {sequence1, reverse(sequence2)};
-        variants.put(subtractionSequences(sequences),sequences);
-        return variants;
-    }
-
-    private static int subtractionSequences(String[] sequences){
-        return Math.abs(Integer.parseInt(sequences[0]) -
-                Integer.parseInt(sequences[1]));
     }
 
     private static boolean checkSequences(String sequence1, String sequence2, String regex){
@@ -64,9 +45,3 @@ public class Sequence {
         return Arrays.equals(seq1, seq2);
     }
 }
-
-// Если последовательности нулей не совпадают, то не совпадают и основные последовательности
-// Переворачиваем только там, где между единицами есть единицы
-// Переворачием только одну, переворачиваем и сравниваем
-
-
